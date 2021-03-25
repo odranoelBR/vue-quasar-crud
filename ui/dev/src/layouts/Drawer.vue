07451a0eea1938fb24b822f518d882c842cf2937
leonzerphio <leonzephiro@gmail.com>
2021-03-25 16:07:49 -0300

    - styling the documentation

---
+++ ui/dev/src/layouts/Drawer.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-drawer
+    v-model="leftDrawerOpen"
+    bordered
+    elevated
+  >
+    <q-list
+      padding
+      class="text-weight-light drawer"
+      separator
+    >
+
+      <q-item
+        v-for="(link, indexRoutes) in routes"
+        :key="indexRoutes"
+        exact
+        :to="{ name: link.routeName }"
+        link
+        active-class="router-link-active"
+      >
+        <q-item-section avatar>
+          <q-icon
+            color="secondary"
+            :name="link.faIcon"
+            alt=""
+          />
+        </q-item-section>
+        <q-item-section
+          label
+          class="light-paragraph"
+        >
+          {{ link.name }}
+        </q-item-section>
+      </q-item>
+
+      <q-expansion-item label="External Links">
+        <q-list no-border>
+          <q-item
+            v-for="(link, indexRoutes) in links"
+            :key="indexRoutes"
+            clickable
+            v-ripple
+            link
+          >
+            <q-item-section>
+
+              <a
+                :href="link.route"
+                target="_blank"
+              >{{link.name}}</a>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-expansion-item>
+    </q-list>
+
+  </q-drawer>
+</template>
+<script>
+export default {
+  components: {
+  },
+  data: () => ({
+    leftDrawerOpen: true,
+    routes: [
+      { route: "/", faIcon: "fa fa-home", materialIcon: "started", name: "Getting started", routeName: "started" },
+      { route: "/more-examples", faIcon: "fa fa-chalkboard-teacher", materialIcon: "data", name: "More examples", routeName: "more-examples" }
+    ],
+    links: [
+      { route: "https://vuejs.org", faIcon: "fa fa-money-bill-alt", name: "Vue.js" },
+      { route: "https://quasar.dev", faIcon: "fa fa-arrows-alt", name: "Quasar.js" },
+      { route: "https://github.com/axios/axios", faIcon: "fa fa-arrows-alt", name: "Axios.js" }
+    ]
+  }),
+  created () {
+  },
+  methods: {
+
+  }
+};
+</script>
+<style scoped>
+.router-link-active {
+  border-right: 3px solid #eb8330;
+  color: #eb8330;
+  background-color: #fafafa;
+}
+.drawer {
+  font-size: 1.1em;
+}
+</style>
---
+++ ui/dev/src/pages/PageMoreExamples.vue
@@ -0,0 +1,143 @@
+<template>
+  <q-page
+    padding
+    class="row justify-center"
+  >
+    <div>
+      <q-tabs
+        v-model="tab"
+        align="justify"
+        narrow-indicator
+        class="bg-grey-2 text-teal"
+      >
+        <q-tab
+          class="text-purple"
+          name="mails"
+          label="Example"
+        />
+        <q-tab
+          class="text-orange"
+          name="alarms"
+          label="Template"
+        />
+        <q-tab
+          class="text-teal"
+          name="movies"
+          label="Script"
+        />
+      </q-tabs>
+      <div>
+        <q-tab-panels
+          keep-alive
+          v-model="tab"
+          animated
+          transition-prev="scale"
+          transition-next="scale"
+        >
+          <q-tab-panel
+            name="mails"
+            ref="panel"
+          >
+            <crud
+              :columns.sync="columns"
+              :http="axios"
+              :can-edit="true"
+              :can-create="true"
+              :can-delete="true"
+              :get-on-start="true"
+              :list-index="list => list.data"
+              :visible-columns="['email']"
+              api="api/users"
+              title="Emails"
+              row-key="id"
+            />
+          </q-tab-panel>
+
+          <q-tab-panel name="alarms">
+            <highlightjs
+              language="htmlbars"
+              :code="code"
+            />
+          </q-tab-panel>
+
+          <q-tab-panel name="movies">
+            <highlightjs
+              language="javascript"
+              :code="columnsString"
+            />
+          </q-tab-panel>
+        </q-tab-panels>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import Crud from '../../../src/components/Crud'
+
+import axios from 'axios'
+
+export default {
+  components: {
+    Crud
+  },
+  created () {
+    this.axios = axios.create({ baseURL: 'https://reqres.in/' })
+  },
+  data: () => ({
+    tab: 'mails',
+    axios: null,
+    code: `<crud
+ :columns.sync="columns"
+ :http="axios"
+ :can-edit="true"
+ :can-create="true"
+ :can-delete="true"
+ :get-on-start="true"
+ :list-index="list => list.data"
+ :visible-columns="['email']"
+ api="api/users"
+ title="Emails"
+ row-key="id"
+/>`,
+    columns: [
+      {
+        name: 'first_name',
+        required: true,
+        label: 'Name',
+        align: 'left',
+        field: 'first_name',
+        sortable: true,
+        type: 'QInput',
+        value: '',
+        size: '6',
+        rules: [val => val && val.length > 0 || 'Please type something'],
+        showCreate: true
+      },
+      {
+        name: 'email',
+        required: true,
+        label: 'Email',
+        align: 'center',
+        field: 'email',
+        sortable: true,
+        type: 'QInput',
+        static: true,
+        value: '',
+        size: '6',
+        showCreate: true
+      }
+    ]
+  }),
+  computed: {
+    columnsString () {
+      return 'columns: ' + JSON.stringify(this.columns, null, 2)
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.q-tab-panel
+  padding: 0px
+</style>
